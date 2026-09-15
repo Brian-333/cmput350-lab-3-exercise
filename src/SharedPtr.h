ebf0bc17523498230752deaf43e7481cf79e0aa6
Brian Lin <shihhsu1@ualberta.ca>
2026-09-15 22:20:46 +0000

    feat: implemented everything

--- src/SharedPtr.h
+++ src/SharedPtr.h
@@ -1,12 +1,15 @@
 #ifndef SHARED_PTR_HEADER
 #define SHARED_PTR_HEADER
 
+#include <utility>
+#include <cassert>
+
 class ControlBlockBase {
 public:
-    ControlBlockBase(); // TODO: implement the default constructor.
+    ControlBlockBase() : currentRefCount(0) {};
 
     // dtor is virtual, so that we can call derived class's dtor from a ptr to this base class.
-    virtual ~ControlBlockBase(); // TODO: implement the destructor.
+    virtual ~ControlBlockBase() {};
 
     // pure virtual function; must be overriden by derived classes
     virtual void* managedAddress() = 0;
@@ -17,21 +20,173 @@ public:
 
     long increment()
     {
-        // TODO: increment refcount by 1 and return result.
+        return ++currentRefCount;
     }
 
     long decrement()
     {
-        // TODO: decrement refcount by 1 and return result.
+        return --currentRefCount;
     }
 
     long refCount() const
     {
-        // TODO: just return the refcount.
+        return currentRefCount;
     }
 
 private:
-    // TODO: add field(s) which both control block types need to have
+    long currentRefCount;
 };
+
+
+template <typename T>
+class ControlBlock : public ControlBlockBase {
+public:
+    explicit ControlBlock(T* inputManagedAddress = nullptr) : currentManagedAddress(inputManagedAddress) {};
+    ~ControlBlock() override { delete currentManagedAddress; };
+    void* managedAddress() override { return currentManagedAddress; };
+private:
+    T* currentManagedAddress;
+};
+
+template <typename T>
+class ControlBlockEmbedded : public ControlBlockBase {
+public:
+    template <typename... Args>
+    explicit ControlBlockEmbedded(Args&&... args) : currentValue(std::forward<Args>(args)...) {};
+    ~ControlBlockEmbedded() override = default;
+    void* managedAddress() override { return &currentValue; };
+private:
+    T currentValue;
+};
+
+template <typename T>
+class SharedPtr {
+public:
+    // Default constructor.
+    explicit SharedPtr() : controlBlock(nullptr), managedAddress(nullptr) {};
+    // Constructor for a new control block
+    explicit SharedPtr(T* managedAddress) : controlBlock(new ControlBlock<T>(managedAddress)), managedAddress(managedAddress) {
+        controlBlock->increment();
+    };
+    template <typename U, typename... Args> friend SharedPtr<U> makeShared(Args&&... args);
+    
+    // Destructor.
+    ~SharedPtr() {
+        reset();
+    };
+    // Copy constructor.
+    SharedPtr(const SharedPtr<T>& other) : controlBlock(other.controlBlock), managedAddress(other.managedAddress) {
+        controlBlock->increment();
+    };
+    // Copy assignment operator.
+    SharedPtr<T>& operator=(const SharedPtr<T>& other) {
+        if (this == &other) {
+            return *this;
+        }
+        // Decrement the ref count of the current control block.
+        reset();
+        // Assign the new control block and managed address.
+        controlBlock = other.controlBlock;
+        managedAddress = other.managedAddress;
+        controlBlock->increment();
+        return *this;
+    };
+    // Move constructor.
+    SharedPtr(SharedPtr<T>&& other) : controlBlock(other.controlBlock), managedAddress(other.managedAddress) {
+        other.controlBlock = nullptr;
+        other.managedAddress = nullptr;
+    };
+    // Move assignment operator.
+    SharedPtr<T>& operator=(SharedPtr<T>&& other) {
+        if (this == &other) {
+            return *this;
+        }
+        // Decrement the ref count of the current control block.
+        reset();
+        // Assign the new control block and managed address.
+        controlBlock = other.controlBlock;
+        managedAddress = other.managedAddress;
+        other.controlBlock = nullptr;
+        other.managedAddress = nullptr;
+        return *this;
+    };
+
+    // Dereference operator.
+    T& operator*() const {
+        assert(managedAddress != nullptr);
+        return *managedAddress;
+    };
+
+    T* operator->() const {
+        assert(managedAddress != nullptr);
+        return managedAddress;
+    };
+
+    // Get the managed address.
+    T* get() const {
+        return managedAddress;
+    };
+
+    bool operator==(const SharedPtr<T>& other) const {
+        return managedAddress == other.managedAddress;
+    };
+
+    // Conversion to bool.
+    explicit operator bool() const { return managedAddress != nullptr; };
+
+    void swap(SharedPtr<T>& other) {
+        if (this == &other) {
+            return;
+        }
+        std::swap(controlBlock, other.controlBlock);
+        std::swap(managedAddress, other.managedAddress);
+    };
+
+    void reset() {
+        if (controlBlock) {
+            controlBlock->decrement();
+            if (controlBlock->refCount() == 0) {
+                delete controlBlock;
+            }
+        }
+        controlBlock = nullptr;
+        managedAddress = nullptr;
+    };
+
+    void reset(T* other) {
+        if (other == managedAddress) {
+            return;
+        }
+        // Reset the current control block and managed address.
+        reset();
+        // Assign the new control block and managed address.
+        controlBlock = new ControlBlock<T>(other);
+        managedAddress = other;
+        controlBlock->increment();
+    };
+
+    long useCount() const {
+        return controlBlock ? controlBlock->refCount() : 0;
+    };
+
+private:
+    ControlBlockBase* controlBlock;
+    T* managedAddress{};
+    // Constructor from managed value with existing control block.
+    SharedPtr(T* mangedAddress, ControlBlockBase* controlblock) : controlBlock(controlblock), managedAddress(mangedAddress) {
+        controlBlock->increment();
+    };
+};
+
+template <typename T, typename... Args> SharedPtr<T> makeSharedBasic(Args&&... args) {
+    return SharedPtr<T>(new T(std::forward<Args>(args)...));
+};
+
+template <typename T, typename... Args> SharedPtr<T> makeShared(Args&&... args) {
+    // Create a new control block for the embedded type.
+    ControlBlockBase* controlBlock = new ControlBlockEmbedded<T>(std::forward<Args>(args)...);
+    // pass the managed address created inside the control block.
+    return SharedPtr<T>(static_cast<T*>(controlBlock->managedAddress()), controlBlock);
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,60 @@
 #include <cstdlib>
 #include "SharedPtr.h"
+#include <iostream>
+#include <cassert>
 
 int main() {
+   // Check for new shared pointer
+   SharedPtr<int> ptr1(new int(10));
+   assert(ptr1.useCount() == 1);
+   assert(*ptr1 == 10);
+
+   // Check for assignment
+   SharedPtr<int> ptr2(new int(20));
+   assert(ptr2.useCount() == 1);
+   ptr1 = ptr2;
+   assert(ptr1.useCount() == 2);
+   assert(*ptr1 == 20);
+   assert(ptr2.get() != nullptr);
+   
+   // Check for makeShared
+   SharedPtr<int> ptr3 = makeShared<int>(30);
+   assert(ptr3.useCount() == 1);
+   assert(*ptr3 == 30);
+
+   // Check for copy constructor
+   SharedPtr<int> ptr4(ptr1);
+   assert(ptr4.useCount() == 3);
+   assert(*ptr4 == 20);
+
+   // check for move constructor
+   SharedPtr<int> ptr5(std::move(ptr1));
+   assert(ptr1.useCount() == 0);
+   assert(ptr1.get() == nullptr);
+   assert(ptr5.useCount() == 3);
+   assert(*ptr5 == 20);
+
+   // check for move assignment
+   ptr1 = std::move(ptr5);
+   assert(ptr1.useCount() == 3);
+   assert(*ptr1 == 20);
+   assert(ptr5.useCount() == 0);
+   assert(ptr5.get() == nullptr);
+
+   // Check for reset
+   ptr1.reset();
+   assert(ptr1.useCount() == 0);
+   assert(ptr1.get() == nullptr);
+
+   // Check for dealing with both embedded and non-embedded types
+   SharedPtr<int> ptr6 = makeSharedBasic<int>(40);
+   SharedPtr<int> ptr7 = makeShared<int>(50);
+   ptr7 = ptr6;
+   assert(ptr6.useCount() == 2);
+   assert(ptr7.useCount() == 2);
+   assert(*ptr6 == 40);
+   assert(*ptr7 == 40);
+
+   std::cout << "All tests passed\n";
    return EXIT_SUCCESS;
 }
